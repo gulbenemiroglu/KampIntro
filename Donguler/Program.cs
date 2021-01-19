d7a4c6e9dd23ec2c482aa8082faf42718e463cbe
Gülben Emiroğlu <gulben.emiroglu@gmail.com>
2021-01-19 16:02:41 +0300

    16 Ocak - Ders Sonu Engin Demirog

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java kursu";
+            string kurs4 = "Python";
+
+
+            // array- dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java kursu" ,"Python Kursu","C#"};
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("Ending for loop - and starting foreach loop");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
