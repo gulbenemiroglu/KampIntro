4b61d96e789352b43ba4e09d47cfcba7e94ec160
Gülben Emiroğlu <gulben.emiroglu@gmail.com>
2021-01-23 05:41:25 +0300

    Engin Demiroğ

---
+++ Ref/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int sayimiz = 3;
+
+        Console.WriteLine("Başlangıç değeri: " + sayimiz);
+
+        Kare(sayimiz);
+        Console.WriteLine("Kare() sonrası: " + sayimiz);
+
+        Kare2(ref sayimiz);
+        Console.WriteLine("Kare2() sonrası: " + sayimiz);
+    }
+
+    static int Kare(int sayi)
+    {
+        return (sayi * sayi);
+    }
+
+    static int Kare2(ref int sayi)
+    {
+        return (sayi * sayi);
+    }
+}
